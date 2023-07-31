b59bc051ff397cc5bb37443eb10fc28aa0538ce5
Hyeongjun Kim <hjkim2246@gmail.com>
2023-07-31 16:00:16 +0900

    Modified utils calculate rmse function

--- week16_timeseries_analysis/utils.py
+++ week16_timeseries_analysis/utils.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 
 
-def calculate_rmse(df, target_column):
-    target_df = df[["Passengers", target_column]].dropna()
-    rmse = mean_squared_error(target_df["Passengers"], target_df[target_column], squared=False)
+def calculate_rmse(df, label_column, target_column):
+    target_df = df[[label_column, target_column]].dropna()
+    rmse = mean_squared_error(target_df[label_column], target_df[target_column], squared=False)
     print(f"{target_column} RMSE: {rmse}")
 
 
@@ -19,6 +19,14 @@ def load_air_passengers():
 def load_sunspots():
     df = pd.read_csv("./data/Sunspots.csv", index_col="Date", parse_dates=True)
     threshold = pd.Timestamp("1965-01-01")
+    train_df = df[:threshold]
+    test_df = df[threshold:]
+    return train_df, test_df
+
+
+def load_restaurant():
+    df = pd.read_csv("./data/restaurant.csv", index_col="date", parse_dates=True)
+    threshold = pd.Timestamp("2016-12-31")
     train_df = df[:threshold]
     test_df = df[threshold:]
     return train_df, test_df
